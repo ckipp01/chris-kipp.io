800774782d5b65c311d35176a4d92da3611e6639
Chris Kipp <ckipp@pm.me>
2023-09-08 08:33:03 +0200

    also bump version of scala that we build with

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,4 @@
-//> using scala "3.3.0"
+//> using scala "3.3.1"
 //> using options "-deprecation", "-feature", "-explain", "-Wunused:all"
 //> using lib "org.scala-lang::scala3-compiler:3.3.1"
 //> using lib "com.lihaoyi::os-lib:0.9.1"
